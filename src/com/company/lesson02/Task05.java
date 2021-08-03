332c08c0500132a82eb0d0333b04aee66c786b30
Mikhail Klimau <mihas90@mail.ru>
2021-08-03 17:38:23 +0300

    commit change

--- src/com/company/lesson02/Task05.java
+++ src/com/company/lesson02/Task05.java
@@ -1,15 +1,18 @@
 package com.company.lesson02;
 
+import java.text.DecimalFormat;
+
 public class Task05 {
     public static void main(String[] args) {
         double a = 2.54;
-        double c = a;
+        double res = a;
+        DecimalFormat df = new DecimalFormat("#0.00");
         for (int b = 1; b <= 20; b++) {
-            System.out.print(b+" дюйм равен " +a);
-            a+=c;
-
-            System.out.println(a);
+            System.out.println(b+" inch = "+df.format(res));
+            res += a;
 
         }
+
+
     }
 }
