bdab098420733072529dc7e89abfc7dcb46058a0
Mikhail Klimau <mihas90@mail.ru>
2021-08-07 13:14:10 +0300

    computer completed

---
+++ src/com/company/lesson4/Computer.java
@@ -0,0 +1,103 @@
+package com.company.lesson4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Computer {
+    String cpu;
+    Integer ram;
+    Integer ssd;
+    Integer resource;
+    int cyclec = 0;
+    boolean isTrue = true;
+    private boolean on = false;
+    private boolean off = true;
+
+
+    public Computer(String cpu, Integer ram, Integer ssd, Integer resource) {
+        this.cpu = cpu;
+        this.ram = ram;
+        this.ssd = ssd;
+        this.resource = resource;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer" + "\n" + '{' +
+                "cpu: " + cpu + '\n' +
+                " ram: " + ram + "\n" +
+                " ssd: " + ssd + "\n" +
+                " resource: " + resource +
+                '}';
+    }
+
+
+    public void isComputerOn() {
+        if (isTrue && off) {
+            int rand;
+            int num;
+            Random random = new Random();
+            rand = random.nextInt(2);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the number 0 or 1:" + rand);
+            num = scanner.nextInt();
+
+
+            if (num == rand) {
+                System.out.println("Is computer on");
+                on = true;
+                off = false;
+
+            } else {
+                System.out.println("This computer burned out");
+                isTrue = false;
+            }
+
+
+        }
+
+    }
+
+    public void isComputeroff() {
+        if (isTrue && on) {
+            int rand;
+            int num;
+            Random random = new Random();
+            rand = random.nextInt(2);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the number 0 or 1:" + rand);
+            num = scanner.nextInt();
+
+
+            if (num == rand) {
+                System.out.println(" Is computer off");
+                off = true;
+                on = false;
+                this.cyclec += 1;
+            } else {
+                System.out.println("This computer burned out");
+                isTrue = false;
+            }
+
+        }
+
+    }
+
+
+    public void isComputerFire() {
+        if (resource <= cyclec) {
+            System.out.println("This computer burned out");
+            this.isTrue = false;
+
+
+        }
+
+
+    }
+
+}
+
+
+
---
+++ src/com/company/lesson4/ComputerMain.java
@@ -0,0 +1,22 @@
+package com.company.lesson4;
+
+public class ComputerMain {
+    public static void main(String[] args) {
+        Computer comp = new Computer("ryzen", 16, 500, 3);
+        System.out.println(comp);
+        for (int i = 0; i <= comp.resource; i++) {
+
+            comp.isComputerOn();
+            comp.isComputeroff();
+            comp.isComputerFire();
+            for (int j = 0; j <= comp.resource; j++) {
+                if (comp.isTrue){
+
+                }else
+                    break;
+            }
+
+        }
+
+    }
+}
